907f17b67d3c8e8c603e424b87581fee0efd46d4
HaedarWinter <407haedar@gmail.com>
2025-06-26 21:30:44 +0700

    Fix 404 page issues with static HTML file approach

--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you were looking for does not exist.</p>
-      <a href="/">Return to Home</a>
-    </div>
-  );
-} 
